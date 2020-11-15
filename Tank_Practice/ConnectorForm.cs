fca2f0a8f3a71432486bab5a5b816626abff0b93
SNMHZ <snmhz325@gmail.com>
2020-11-16 00:06:28 +0900

    p2p socket added

--- Tank_Practice/ConnectorForm.cs
+++ Tank_Practice/ConnectorForm.cs
@@ -8,13 +8,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Tank_Practice
 {
     public partial class ConnectorForm : Form
     {
+        public Form1 parent;
+        bool serverOpen;
+        public bool connectReq;
         public ConnectorForm()
         {
             InitializeComponent();
+            textBox1.Text = "21212";
+            textBox2.Text = "127.0.0.1";
+            serverOpen = connectReq = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            ConnectLoglistBox.Items.Add("Server Open Started.");
+            try
+            {
+                btnConnect.Enabled = false;
+                btnOpen.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+
+                parent.openServer();
+                ConnectLoglistBox.Items.Add("Waiting for other player...");
+                serverOpen = true;
+            }
+            catch(Exception ex)
+            {
+                ConnectLoglistBox.Items.Add("Error! check port number");
+            }
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectLoglistBox.Items.Add("Connect Request Started...");
+            try
+            {
+                btnConnect.Enabled = false;
+                btnOpen.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                parent.connectServer();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Connection Failed! check your Address.");
+            }
+        }
+
+        private void ConnectorForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serverOpen && !connectReq)
+            {
+                parent.server.StopServer();
+            }
         }
     }
 }
